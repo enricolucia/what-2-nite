888dce64d0042e131b11deffcfcca5f77607ece4
enricolucia <enrico.lucia.web@gmail.com>
2015-01-07 15:43:51 +0000

    Persistent angular route on server

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 /*jslint node: true, nomen: true, sloppy: true */
 var express = require('express'),
     langs = require('./isoLangs.json'),
+    path = require('path'),
     bodyParser = require('body-parser'),
     app = express();
 
+app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'app')));
 app.use(bodyParser.json());
-app.use(express.static('app'));
-
 
 app.get('/isolangs.json', function(req, res) {
   res.send(langs);
 });
 
-app.get('/*', function(req, res) {
-  res.redirect('/');
+app.use(function(req, res) {
+  // Use res.sendfile, as it streams instead of reading the file into memory.
+  res.sendFile(__dirname + '/app/index.html');
 });
+
 
 module.exports = app;
